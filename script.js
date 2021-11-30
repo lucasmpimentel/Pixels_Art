be23ad139a80fb7a06a42b73a31d03be05fc1a87
lucasmpimentel <lucas.mirandapimentel@gmail.com>
2021-11-30 01:01:58 -0300

    correçao de bugs no colorir do quadro e na cor 1 que nao pintava com a cor correta, implementado boao limpar

--- script.js
+++ script.js
@@ -1,58 +1,35 @@
-// Declaração de variaveis e constantes.
-let boardSize = document.querySelector('#board-size');
+// --------------- Declarações de interface -------------------------
+// let = document.querySelector('#board-size');
 const firstBoard = 5;
 const pBoard = document.getElementById('pixel-board');
 const newColors = document.querySelector('#restart');
 const clearBt = document.getElementById('clear-board');
-const vqv = document.querySelector('#generate-board');
-const black = document.querySelector('.black');
+// const vqv = document.querySelector('#generate-board');
+const pixels = document.getElementsByClassName('pixel');
 const palettes = document.getElementsByClassName('color');
+
+// ---------------------------- FUNÇÕES ------------------------------
 
 /* Após ler a documentação de Math.floor e Math.random e buscar
 soluções e discutir com colegas, cheguei a essa conclusão para
 gerar cores aleatórias */
-/* pesquisando formas de encurtar a função, achei essas arrow funcions */
+/* pesquisando formas de encurtar a função, achei essas arrow funcions
+https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Functions/Arrow_functions */
+
+// ----------------------------  CORES ALEATÓRIAS ----------------------------
 const randomize = () => Math.floor(Math.random() * (255 - 1) + 1);
 const generatePalette = () => {
-  const pC2 = document.getElementById('color2');
-  pC2.style.backgroundColor = `rgb(${randomize()}, ${randomize()}, ${randomize()})`;
-  const pC3 = document.getElementById('color3');
-  pC3.style.backgroundColor = `rgb(${randomize()}, ${randomize()}, ${randomize()})`;
-  const pC4 = document.getElementById('color4');
-  pC4.style.backgroundColor = `rgb(${randomize()}, ${randomize()}, ${randomize()})`;
+  const c1 = document.querySelector('#color1');
+  c1.style.backgroundColor = 'black';
+  const c2 = document.querySelector('#color2');
+  c2.style.backgroundColor = `rgb(${randomize()}, ${randomize()}, ${randomize()})`;
+  const c3 = document.querySelector('#color3');
+  c3.style.backgroundColor = `rgb(${randomize()}, ${randomize()}, ${randomize()})`;
+  const c4 = document.querySelector('#color4');
+  c4.style.backgroundColor = `rgb(${randomize()}, ${randomize()}, ${randomize()})`;
 };
-const first = document.querySelector('.color');
-first.classList.add('selected');
 
-function getColor(event) {
-  const get = document.querySelector('.selected');
-  get.classList.remove('selected');
-  event.target.classList.add('selected');
-}
-for (let i2 = 0; i2 < palettes.lenght; i2 += 1) {
-  palettes[i2].addEventListener('click', getColor);
-}
-
-function paint(event) {
-  const clicked = event.target;
-  const choosen = document.querySelector('.selected');
-  if (choosen === black) {
-    clicked.classList.remove('clean');
-    clicked.classList.add('black');
-  } else if (choosen === document.getElementById('color2')) {
-    clicked.classList.remove('clean');
-    const choosen2 = document.getElementById('color2').style.backgroundColor;
-    clicked.style.backgroundColor = choosen2;
-  } else if (choosen === document.getElementById('color3')) {
-    clicked.classList.remove('clean');
-    const choosen3 = document.getElementById('color3').style.backgroundColor;
-    clicked.style.backgroundColor = choosen3;
-  } else {
-    clicked.classList.remove('clean');
-    const choosen4 = document.getElementById('color4').style.backgroundColor;
-    clicked.style.backgroundColor = choosen4;
-  }
-}
+// ---------------------------- PRIMEIRO QUADRO ------------------------------
 
 for (let i = 0; i < firstBoard; i += 1) {
   const line = document.createElement('div');
@@ -60,19 +37,55 @@ for (let i = 0; i < firstBoard; i += 1) {
   pBoard.appendChild(line);
   for (let ii = 0; ii < firstBoard; ii += 1) {
     const circle = document.createElement('div');
-    circle.classList.add('pixel', 'clean');
+    circle.classList.add('pixel');
+    circle.style.backgroundColor = 'white';
     line.appendChild(circle);
-    circle.addEventListener('click', paint);
   }
 }
 
+// ------------------------------ SELECINAR PRETO ----------------------------
 
+const first = document.getElementById('color1');
+first.classList.add('selected');
+
+// -------------------------- SELECIONAR COR DA PALETA ------------------------
+
+for (const iii of palettes) {
+  iii.addEventListener('click', (event) => {
+    const lastColor = document.querySelector('.selected');
+    lastColor.classList.remove('selected');
+    event.target.classList.add('selected');
+  });
+}
+
+// -------------------------------- COLORIR PIXELS -----------------------------
+
+for (const iv of pixels) {
+  iv.addEventListener('click', (event) => {
+    const clickedColor = document.querySelector('.selected').style.backgroundColor;
+    event.target.style.backgroundColor = clickedColor;
+  });
+}
+
+// ------------------------------ LIMPAR QUADRO ---------------------------------
 function clearBoard() {
   const board = document.getElementsByClassName('pixel');
   for (let i = 0; i < board.length; i += 1) {
-    board[i].classList.add('clean');
+    board[i].style.backgroundColor = 'white';
   }
 }
+
+// ---------------- EVENTOS --------------------------
+// window.onload = setFirstSelection;
+generatePalette();
+newColors.addEventListener('click', generatePalette);
+clearBt.addEventListener('click', clearBoard);
+
+//
+
+//
+
+//
 
 /* function boardChanger() {
   if (boardSize !== '' && boardSize.value > 5) {
@@ -88,10 +101,3 @@ function clearBoard() {
   return boardSize;
 }
 vqv.addEventListener('click', boardChanger); */
-
-// EVENTOS
-// window.onload = setFirstSelection;
-generatePalette();
-newColors.addEventListener('click', generatePalette);
-clearBt.addEventListener('click', clearBoard);
-
